ec184e7ebf082c7f60e9aa352cd05f6113a6efbe
omoefe <raniem57@gmail.com>
2025-03-31 13:19:11 +0100

    updated

--- tony-poem/vite.config.js
+++ tony-poem/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1600,
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+});
